f426c30a4bb542eeab5eef2a286b0d9b3656ce63
Iraponan Marinho <iraponan.aires@gmail.com>
2024-06-19 23:38:45 -0300

    Realizando o login pelo controller.

--- lib/src/controllers/auth.dart
+++ lib/src/controllers/auth.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/repositories/auth.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
+  final authRepository = AuthRepository();
+
   Future<void> signIn({required String email, required String password}) async {
     isLoading.value = true;
 
-    await Future.delayed(const Duration(seconds: 3));
+    await authRepository.signIn(email: email, password: password);
 
     isLoading.value = false;
   }
